361064b1b4695c04abe3df3896567c2522135fa9
baobao442005 <baongocvien442005@gmail.com>
2025-10-24 22:17:53 +0700

    feature/auth-infra: update vwAccount + user.model.js + account.route.js + db.js + main.handlebar code

--- app.js
+++ app.js
@@ -5,32 +5,62 @@ import session from 'express-session';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { restrictAdmin, checkAuthenticated } from './middlewares/auth.mdw.js';
-
+import accountRouter from './routes/account.route.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.engine('handlebars', engine({
-  helpers: { fillContent: hbs_sections() }
-}));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
+app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'sgghgfgghjgjffhgjgfh',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false }
-}));
+}))
 
+app.engine('handlebars', engine({
+  helpers: { fill_Content: hbs_sections() }
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.urlencoded(
+    {extended:true}
+));
+
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { secure: false }
+// }));
+
+app.use(async function(req, res, next) {
+  if(req.session.isAuthenticated) {
+    res.locals.isAuthenticated = true;
+    res.locals.authUser = req.session.authUser;
+  }
+  //copy toan bo du lieu trong session vao bien cuc bo
+  next();
+});
+
+
+app.use('/account', accountRouter);
 app.get('/', (req, res) => {
+   if(req.session.isAuthenticated){
+    console.log(req.session.authUser);
+    console.log('User is logged in');
+  }
   res.render('vwHome/index');
+});
+
+app.use(function(req, res) {
+    res.status(404).render('404');
 });
 
 app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
---
+++ utils/db.js
@@ -0,0 +1,17 @@
+// utils/database.js
+import knex from 'knex';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'aws-1-ap-southeast-1.pooler.supabase.com',
+    port: 5432,
+    user: 'postgres.anvurmbutyvmzanonbyh',
+    password: 'Phucle@2608',
+    database: 'postgres',
+    ssl: { rejectUnauthorized: false } 
+  },
+  pool: { min: 0, max: 15 },
+});
+
+export default db;
