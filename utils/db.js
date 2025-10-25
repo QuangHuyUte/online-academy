f3b10bd69c35dd30c120b1657a5789349bcc7a84
Chung Trường Vũ <23110072@student.hcmute.edu.vn>
2025-10-25 16:19:37 +0700

    hoàn tất chức năng search+lọc+paging+badge

---
+++ utils/db.js
@@ -0,0 +1,14 @@
+import knex from 'knex';
+const db = knex({
+        client: 'pg',
+        connection: {
+            host: 'db.eaiaaadpuhpimnefmgac.supabase.co',
+            port: 5432,
+            user: 'postgres',
+            password:'truongvu123@#*',
+            database: 'postgres'
+        },
+        pool: { min: 0, max: 15}
+    });
+
+export default db;
